46e58e2d3380f23c78c0bdd29ff48b24d7d2de87
IT21333620 <it21333620@my.sliit.lk>
2023-04-08 13:58:16 +0530

    2nd

---
+++ as.js
@@ -0,0 +1,14 @@
+console.log("razer");
+console.log("razer");
+console.log("razer");
+console.log("razer");
+
+console.log("razer");
+
+console.log("razer");
+console.log("razer");
+console.log("razer");
+console.log("razer");
+console.log("razer");
+console.log("razer");
+console.log("razer");
